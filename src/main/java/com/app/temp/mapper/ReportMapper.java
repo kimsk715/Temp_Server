25374303587641dcda5dd8d78461844b2e12d6d3
JisooHahn <luna99954@gmail.com>
2025-03-21 16:51:16 +0900

    16:49 신고 분리, 필터 작업중

--- src/main/java/com/app/temp/mapper/ReportMapper.java
+++ src/main/java/com/app/temp/mapper/ReportMapper.java
@@ -1,9 +1,6 @@
 package com.app.temp.mapper;
 
-import com.app.temp.domain.dto.CompanyReportDTO;
-import com.app.temp.domain.dto.Pagination;
-import com.app.temp.domain.dto.ReportInfoDTO;
-import com.app.temp.domain.dto.ReportListDTO;
+import com.app.temp.domain.dto.*;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.ArrayList;
@@ -22,9 +19,33 @@ public interface ReportMapper {
     //  전체 개수 조회
     public int selectTotal(Pagination pagination);
 
-    //  신고 id로 상세정보 조회
-    public Optional<ReportInfoDTO> selectReportDetailById(Long id);
+    //  기업 신고 전체 개수 조회
+    public int selectCompanyReportTotal(Pagination pagination);
+
+    //  공고 신고 전체 개수 조회
+    public int selectProgramReportTotal(Pagination pagination);
+
+    //  신고 id로 기업 상세정보 조회
+    public Optional<ReportInfoDTO> selectCompanyReportDetailById(Long id);
+
+    //  신고 id로 공고 상세정보 조회
+    public Optional<ReportInfoDTO> selectProgramReportDetailById(Long id);
 
     //  신고 처리상태 수정
     public void updateReportStatus(ReportInfoDTO reportInfoDTO);
+
+    //  신고 작성하기
+    public void insertReport(ReportInfoDTO reportInfoDTO);
+
+    //  기업신고 작성하기(기업신고 테이블만)
+    public void insertCompanyReport(CompanyReportInfoDTO companyReportInfoDTO);
+
+    //  공고신고 작성하기(공고신고 테이블만)
+    public void insertProgramReport(ProgramReportInfoDTO programReportInfoDTO);
+
+    //  기업 신고 전체 목록 조회
+    public List<CompanyReportInfoDTO> selectAllCompanyReport(Pagination pagination);
+
+    //  공고 신고 전체 목록 조회
+    public List<ProgramReportInfoDTO> selectAllProgramReport(Pagination pagination);
 }
--- src/main/resources/static/js/admin/reportService.js
+++ src/main/resources/static/js/admin/reportService.js
@@ -1,12 +1,12 @@
 const reportService = (() => {
 
-    // 신고 목록
+    // 기업 신고 목록
     // 페이지 기본값은 1, callback은 데이터를 가져온 후에 실행할 함수
-    const getList = async (callback, param={}) => {
+    const getCompanyList = async (callback, param={}) => {
         let page = param.page || 1;
         let search = param.search;
 
-        let path = `/admin/reports/list?page=${page}`;    // 기본 경로
+        let path = `/admin/home/reports/company-list?page=${page}`;    // 기본 경로
 
         // 검색 조건 추가 (keyword, reportStatus, createdDateStart, createdDateEnd)
         if (search) {
@@ -29,30 +29,117 @@ const reportService = (() => {
         const response = await fetch(path);
 
         // 응답 데이터를 JSON 형태로 반환
-        const reportListData = await response.json();
-
+        const data = await response.json();
+        const companyReportListData = data.companyReports;
+        const companyPagination = data.pagination;
         // 콜백 함수가 존재한다면
         if(callback){
             // 데이터를 전달해 실행
-            console.log("콜백 실행됨");
-            callback(reportListData);
+            console.log("콜백 실행됨")
+            callback(companyReportListData, companyPagination);
         }
     }
 
+    // 공고 신고 목록
+    // 페이지 기본값은 1, callback은 데이터를 가져온 후에 실행할 함수
+    const getProgramList = async (callback, param={}) => {
+        let page = param.page || 1;
+        let search = param.search;
+
+        let path = `/admin/home/reports/program-list?page=${page}`;    // 기본 경로
+
+        // 검색 조건 추가 (keyword, reportStatus, createdDateStart, createdDateEnd)
+        if (search) {
+            if (search.keyword) {
+                path += `&keyword=${search.keyword}`;  // 키워드
+            }
+            if (search.reportStatus) {
+                path += `&reportStatus=${search.reportStatus}`;  // 처리상태
+            }
+            if (search.createdDateStart) {
+                path += `&createdDateStart=${search.createdDateStart}`;  // 시작 날짜
+            }
+            if (search.createdDateEnd) {
+                path += `&createdDateEnd=${search.createdDateEnd}`;  // 종료 날짜
+            }
+        }
+
+        // 서버에 요청(fetch)
+        console.log("Fetch 시작");
+        const response = await fetch(path);
+
+        // 응답 데이터를 JSON 형태로 반환
+        const data = await response.json();
+        const programReportListData = data.programReports;
+        const programPagination = data.pagination;
+        // 콜백 함수가 존재한다면
+        if(callback) {
+            // 데이터를 전달해 실행
+            console.log("콜백 실행됨")
+            callback(programReportListData, programPagination);
+            console.log(programReportListData, programPagination);
+        }
+    }
+
+    // 기업신고 추가
+    const writeCompanyReport = async (report) => {
+        await fetch("/report/company-create", {
+            method: "POST",
+
+            body: JSON.stringify(report),
+            headers: {
+                "Content-Type": "application/json;charset-utf-8"
+            }
+        });
+    }
+
+    // 공고신고 추가
+    const writeProgramReport = async (report) => {
+        await fetch("/report/program-create", {
+            method: "POST",
+
+            body: JSON.stringify(report),
+            headers: {
+                "Content-Type": "application/json;charset-utf-8"
+            }
+        });
+    }
+
+    // 신고하기 눌렀을 때 로그인 검사
+    const checkReportLogin = async () => {
+        const response = await fetch("/member/check-login", {method: "GET"});
+        const data = await response.json();
+        return data;
+    };
+
     // 모듈에서 사용할 메소드를 객체로 반환
-    return {getList : getList};
+    return {getCompanyList : getCompanyList, getProgramList : getProgramList, writeCompanyReport : writeCompanyReport, writeProgramReport : writeProgramReport, checkReportLogin : checkReportLogin};
 })();
 
 // 신고 상세 조회 추가
 const reportModalService = (() => {
 //     상세 정보(데이터) 가져오기
-    const getReportDetail = async (reportId) => {
-        const response = await fetch(`/admin/reports/${reportId}`);
+    const getCompanyReportDetail = async (reportId) => {
+        const response = await fetch(`/admin/home/reports/company/${reportId}`);
+        return response.json(); // 서버에서 받은 json 형태의 데이터
+    }
+    const getProgramReportDetail = async (reportId) => {
+        const response = await fetch(`/admin/home/reports/program/${reportId}`);
         return response.json(); // 서버에서 받은 json 형태의 데이터
     }
     // 모달 저장 버튼 클릭 시 처리 상태 저장
-    const updateStatus = async (report) => {
-        const response = await fetch("/admin/reports/update", {
+    const updateCompanyStatus = async (report) => {
+        const response = await fetch("/admin/home/reports/company/update", {
+            method: 'put',
+            body: JSON.stringify(report),
+            headers: {
+                "Content-Type": "application/json;charset=utf-8"
+            }
+        });
+        return response.json();
+    }
+    const updateProgramStatus = async (report) => {
+        const response = await fetch("/admin/home/reports/program/update", {
             method: 'put',
             body: JSON.stringify(report),
             headers: {
@@ -62,7 +149,7 @@ const reportModalService = (() => {
         return response.json();
     }
 
-    return {getReportDetail : getReportDetail, updateStatus: updateStatus}
+    return {getCompanyReportDetail : getCompanyReportDetail, getProgramReportDetail : getProgramReportDetail, updateCompanyStatus: updateCompanyStatus, updateProgramStatus : updateProgramStatus}
 })();
 
 
